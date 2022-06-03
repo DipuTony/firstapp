3ed9da52807793d3abdb035ad98fedaaa97f1853
Dipu Singh <dsingh197@gmail.com>
2022-06-03 23:31:51 +0530

    apply State

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    const [text, setText] = useState("Enter Text here");
     return (
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="6"></textarea>
+                <textarea className="form-control" value={text} id="myBox" rows="6"></textarea>
             </div>
             <button className="btn btn-primary">Convert Upper Case</button>
         </div>
