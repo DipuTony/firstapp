2660ffacca43221295709d1fe7828c996bfe7fe0
Dipu Singh <dsingh197@gmail.com>
2022-06-03 22:43:34 +0530

    TestFrom-Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 let myname = "Dipu Singh";
 function App() {
   return (
    <>
     <Navbar title="TextUtils"/>
+    <div className="container">
+      <TextForm/>
+    </div>
    </>
   );
 }
