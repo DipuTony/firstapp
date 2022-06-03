1a7fb508620e70a9147f1b61396f96a4591f57e4
Dipu Singh <dsingh197@gmail.com>
2022-06-03 23:41:35 +0530

    State setText Function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+
+    const handleUpClick = () =>{
+        console.log("upper Case was clicked");
+        setText("You have clicked on Button and HandleUpClik function Run")
+    }
+
+    const handleOnChange = () =>{
+        console.log("On Change");
+    }
+
     const [text, setText] = useState("Enter Text here");
     return (
         <div>
@@ -8,7 +18,7 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} id="myBox" rows="6"></textarea>
             </div>
-            <button className="btn btn-primary">Convert Upper Case</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert Upper Case</button>
         </div>
     )
 }
