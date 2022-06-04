c341ad83fdf31b1560a2464a84e7dc1050c6db91
Dipu Singh <dsingh197@gmail.com>
2022-06-04 14:25:55 +0530

    Navbar Dark/Light Mode with Switch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
@@ -5,9 +6,20 @@ import TextForm from './components/TextForm';
 
 let myname = "Dipu Singh";
 function App() {
+  const [mode, setMode] = useState('light'); //Wheter Dark mode is enable or not
+  //create function for toogle dark/light mode
+  const toogelMode = () =>{
+    if(mode=== 'light'){
+      setMode('dark');
+      // console.log(mode);
+    }else{
+      setMode('light');
+      // console.log(mode);
+    }
+  }
   return (
    <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode} toogelMode={toogelMode}/>
     <div className="container my-3">
       <TextForm heading="Enter the text to analyze." />
       <About/>
